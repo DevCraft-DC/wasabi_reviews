a233385e6440f9f832a0061587921201da6a18fe
tepsurkaev <unknownsayid@mail.ru>
2023-03-27 23:39:56 +0300

    Resolved merge conflicts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from 'path';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "/wasabi_reviews/"
-})
+  plugins: [react(), tsconfigPaths()],
+  base: '/wasabi_reviews/',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src')
+    }
+  }
+});
