d5f38ebed69fcc614ac359680615090ddc065e22
tepsurkaev <unknownsayid@mail.ru>
2023-03-29 11:22:29 +0300

    Layout component completed

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,6 +1,6 @@
 import type { ReactNode } from 'react';
 
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 
 import styles from './layout.module.scss';
 import logo from '../../assets/logo.svg';
@@ -9,10 +9,17 @@ interface LayoutProps {
   searchComponent?: ReactNode;
 }
 
-export const Layout = ({ searchComponent }: LayoutProps) => (
-  <div className={styles.layout}>
-    <img src={logo} alt="logo" />
-    {searchComponent !== undefined && searchComponent}
-    <Outlet />
-  </div>
-);
+export const Layout = ({ searchComponent }: LayoutProps) => {
+  const { pathname } = useLocation();
+  const isFoodPage = pathname.includes('food');
+
+  return (
+    <div className={styles.layout}>
+      <div className={styles.layout_wrapper}>
+        <img className={styles.logo} src={logo} alt="logo" />
+        {searchComponent !== undefined && !isFoodPage && searchComponent}
+      </div>
+      <Outlet />
+    </div>
+  );
+};
