a3c8dbd97881fedd18f255e37a56108b75f1caf1
Muhammad-Nasyr <nasirmaga45@gmail.com>
2023-03-29 13:55:05 +0300

    prettier fix

--- src/pages/Staff/Staff.tsx
+++ src/pages/Staff/Staff.tsx
@@ -4,17 +4,17 @@ import staff from '../../assets/staff.json';
 import { Card } from '@/components/Card/Card';
 
 const Staff = () => (
-    <div className={styles.container}>
-      {staff.map((member) => (
-          <div key={member.name}>
-            <Card>
-              <img src={member.image} alt={member.name} />
-              <p>{member.position}</p>
-              <h3>{member.name}</h3>
-            </Card>
-          </div>
-        ))}
-    </div>
-  );
+  <div className={styles.container}>
+    {staff.map((member) => (
+      <div key={member.name}>
+        <Card>
+          <img src={member.image} alt={member.name} />
+          <p>{member.position}</p>
+          <h3>{member.name}</h3>
+        </Card>
+      </div>
+    ))}
+  </div>
+);
 
 export default Staff;
