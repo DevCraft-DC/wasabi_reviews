92c7868cdaabc8ce9cc17f79d74a389d01db2ef9
Muhammad-Nasyr <nasirmaga45@gmail.com>
2023-04-04 20:11:45 +0300

    thanks page done

--- src/pages/Thanks/Thanks.tsx
+++ src/pages/Thanks/Thanks.tsx
@@ -1,5 +1,13 @@
 import styles from './thanks.module.scss';
 
-const Thanks = () => <h1 className={styles.remove}>Thanks Page</h1>;
+const Thanks = () => (
+  <div className={styles.container}>
+    <h1 className={styles.title}>Спасибо за отзыв!</h1>
+    <p className={styles.text}>
+      Мы ценим ваше мнение и рады, что вы нашли время на оставление отзыва. Ваши пожелания будут
+      рассмотрены и учтены. Ждём вас в скорейшем времени.
+    </p>
+  </div>
+);
 
 export default Thanks;
