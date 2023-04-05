bfa9bb5f384c2e1707fdb252d8989c51ab30bb37
Muhammad-Nasyr <nasirmaga45@gmail.com>
2023-04-05 16:23:50 +0300

    fixed choose page

--- src/pages/Choose/Choose.tsx
+++ src/pages/Choose/Choose.tsx
@@ -1,17 +1,53 @@
 import { Link } from 'react-router-dom';
 
 import styles from './choose.module.scss';
-import { Card } from './components/Card/Card';
+import { Card } from '../../components/Card/Card';
 
-const Choose = () => (
-  <>
-    <div className={styles.main}>
-      <Card content="Блюда" />
-      <Card content="Персонал" />
-      <Card content="Другое" />
-    </div>
-    <Link to="#">Назад</Link>
-  </>
-);
+const Choose = () => {
+  const saveCategory = (catName: string) => {
+    localStorage.setItem('category', catName);
+  };
+
+  return (
+    <>
+      <div className={styles.main}>
+        <h4 className={styles.page_text}>О чём вы хотите оставить отзыв?</h4>
+
+        <Link to="/wasabi_reviews/food">
+          <Card
+            onClick={() => {
+              saveCategory('food');
+            }}
+            className={styles.choose_card}
+          >
+            <span>Еда</span>
+          </Card>
+        </Link>
+
+        <Link to="/wasabi_reviews/staff">
+          <Card
+            onClick={() => {
+              saveCategory('staff');
+            }}
+            className={styles.choose_card}
+          >
+            <span>Персонал</span>
+          </Card>
+        </Link>
+
+        <Link to="/wasabi_reviews/other">
+          <Card
+            onClick={() => {
+              saveCategory('other');
+            }}
+            className={styles.choose_card}
+          >
+            <span>Другое</span>
+          </Card>
+        </Link>
+      </div>
+    </>
+  );
+};
 
 export default Choose;
--- src/pages/Choose/components/Card/Card.tsx
+++
@@ -1,9 +0,0 @@
-import type { ReactNode } from 'react';
-
-import styles from './card.module.scss';
-
-interface CardProps {
-  content: ReactNode;
-}
-
-export const Card = ({ content }: CardProps) => <div className={styles.card}>{content}</div>;
