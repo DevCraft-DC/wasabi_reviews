53f8214e7e58088d8c90cd0dea2ec29c8b1b5f12
Magomed <algiraev@MacBook-Air-Magomed.local>
2023-04-06 17:31:59 +0300

    took the input from layout to food page

--- src/pages/Food/Food.tsx
+++ src/pages/Food/Food.tsx
@@ -1,17 +1,21 @@
 import styles from './food.module.scss';
 import food from '../../assets/food.json';
 
+import { SearchInput } from '@/components/SearchInput/SearchInput';
 import { Card } from '@/components/Card/Card';
 
 const Food = () => (
   <div className={styles.page_container}>
-    <h4 className={styles.pageText}>
+    <h4 className={styles.page_text}>
       Выберите блюдо из наешго меню,
       <br /> которое хотели бы оценить:
     </h4>
-    <div className={styles.cardsContainer}>
+
+    <SearchInput />
+
+    <div className={styles.cards_container}>
       {food.map((food) => (
-        <Card key={food.title} className={styles.staffCard}>
+        <Card key={food.title} className={styles.staff_card}>
           <img src={food.image} alt={food.title} />
           <p>{food.title}</p>
         </Card>
