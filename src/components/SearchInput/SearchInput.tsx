787a1246ca8375db3d1deffa87e6bc5994fa3017
Magomed <algiraev@MacBook-Air-Magomed.local>
2023-04-06 14:59:39 +0300

    Finished food page

--- src/components/SearchInput/SearchInput.tsx
+++ src/components/SearchInput/SearchInput.tsx
@@ -1,9 +1,22 @@
+import { useState } from 'react';
+
 import styles from './searchInput.module.scss';
 import searchIcon from '../../assets/search.svg';
 
-export const SearchInput = () => (
-  <div className={styles.search_wrapper}>
-    <input className={styles.search_input} type="text" placeholder="Введите название блюда" />
-    <img className={styles.search_icon} src={searchIcon} alt="search icon" />
-  </div>
-);
+export const SearchInput = () => {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [search, setSearch] = useState('');
+  return (
+    <div className={styles.search_wrapper}>
+      <input
+        className={styles.search_input}
+        type="text"
+        placeholder="Введите название блюда"
+        onChange={(e) => {
+          setSearch(e.target.value);
+        }}
+      />
+      <img className={styles.search_icon} src={searchIcon} alt="search icon" />
+    </div>
+  );
+};
