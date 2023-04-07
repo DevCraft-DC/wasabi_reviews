f60c109eec006d92831113d5ac0fe9b7137fff3a
Muhammad-Nasyr <nasirmaga45@gmail.com>
2023-04-07 15:38:07 +0300

    implemented search on food page, added modal window, fixed cards

--- src/pages/Food/Food.tsx
+++ src/pages/Food/Food.tsx
@@ -1,29 +1,60 @@
+import { useState } from 'react';
+
 import styles from './food.module.scss';
 import food from '../../assets/food.json';
 
 import { SearchInput } from '@/components/SearchInput/SearchInput';
+import { ReviewModal } from '@/components/ReviewModal/ReviewModal';
 import { Card } from '@/components/Card/Card';
 
-const Food = () => (
-  <div className={styles.page_container}>
-    <h4 className={styles.page_text}>
-      Выберите блюдо из наешго меню,
-      <br /> которое хотели бы оценить:
-    </h4>
+const Food = () => {
+  const [searchText, setSearchText] = useState('');
+  const [modal, setModal] = useState(false);
+  const [title, setTitle] = useState('');
 
-    <SearchInput />
+  const showModal = (foodTitle: string) => {
+    setModal(true);
+    setTitle(foodTitle);
+    localStorage.setItem('target', foodTitle);
+  };
 
-    <div className={styles.cards_container}>
-      {food.map((food) => (
-        <Card key={food.title} className={styles.dish_card}>
-          <img src={food.image} alt={food.title} />
-          <div className={styles.data}>
-            <p>{food.title}</p>
-          </div>
-        </Card>
-      ))}
+  const filteredFood = food.filter((dish) =>
+    dish.title.toUpperCase().includes(searchText.toUpperCase())
+  );
+
+  return (
+    <div className={styles.page_container}>
+      <ReviewModal
+        reviewTarget={title}
+        show={modal}
+        removeModal={() => {
+          setModal(false);
+        }}
+      />
+      <h4 className={styles.page_text}>
+        Выберите блюдо из нашего меню, которое хотели бы оценить:
+      </h4>
+
+      <SearchInput searchText={searchText} setSearchText={setSearchText} />
+
+      <div className={styles.cards_container}>
+        {filteredFood.map((food) => (
+          <Card
+            onClick={() => {
+              showModal(food.title);
+            }}
+            key={food.title}
+            className={styles.dish_card}
+          >
+            <img src={food.image} alt={food.title} />
+            <div className={styles.data}>
+              <h3>{food.title}</h3>
+            </div>
+          </Card>
+        ))}
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Food;
